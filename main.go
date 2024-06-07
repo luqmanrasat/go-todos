d31fdb688c0eaff3e28df913b9432e0269a9ed14
Luqman Rasat <luqmanrasat@hotmail.com>
2024-06-08 07:10:11 +0800

    defer close db connection

--- main.go
+++ main.go
@@ -42,6 +42,7 @@ func main() {
 	// TODO: check if error connecting to DB
 	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(POSTGRES_URI)))
 	db = bun.NewDB(sqldb, pgdialect.New())
+	defer db.Close()
 
 	// TODO: add migration
 	// _, err = db.NewCreateTable().Model((*Todo)(nil)).Exec(context.Background())
